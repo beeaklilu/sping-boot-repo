5ccc07836113d3dc61424b947ee08f464dc0acde
beeAk <se.blen.aklilu@gmail.com>
2021-04-23 07:32:54 -0500

    initial commit'

---
+++ LabThree/LabThree/src/test/java/com/example/labthree/LabThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
